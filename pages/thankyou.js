d635e860d2a9ffb2d8fefde0eadedd661d16d7d3
ugphone839 <testugphone3.6@gmail.com>
2025-11-24 11:49:16 +0700

    Update thankyou.js

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -2,12 +2,12 @@ export default function ThankYou({ coin }) {
     return (
         <div style={{ textAlign: "center", marginTop: "60px" }}>
             <h1>Cảm ơn bạn đã hoàn thành link!</h1>
-            <h2>Bạn đã nhận: +{coin} coin</h2>
+            <h2>Bạn đã nhận +{coin} coin</h2>
             <p>Hãy quay lại Discord để kiểm tra số dư.</p>
         </div>
     );
 }
 
 ThankYou.getInitialProps = ({ query }) => {
-    return { coin: query.coin || 0 }
-}
+    return { coin: query.coin || 0 };
+};
